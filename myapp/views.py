92986e19aea88374618ac2b18c69cd6fcb890836
KeiZaby7 <keizaby7@gmail.com>
2022-09-18 10:26:19 -0300

    Primer Commit hasta Params

---
+++ myapp/views.py
@@ -0,0 +1,11 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def hello(request):
+    return HttpResponse("<h1>Hello World</h1>")
+
+def about(request):
+    return HttpResponse("About")
